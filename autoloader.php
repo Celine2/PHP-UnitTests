094dea4c0a15c4bf4ae82562c6861388ac41f2d1
Hans Vanpée <hans@vanpee.be>
2018-05-22 09:51:31 +0200

    Add autoloader and index

---
+++ autoloader.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * A simple autoloader function, PSR4 compliant.
+ *
+ * Namespace components are converted into sub-directories, all characters
+ * are converted to lowercase.
+ */
+
+spl_autoload_register('Autoloader::class_loader');
+include_once 'vendor/autoload.php';
+
+class AutoLoader
+{
+    public static function class_loader(string $class) : void
+    {
+        // Adapt the path to your class directory
+        // The prefix is different for cli invocations, so we check this first.
+        if (php_sapi_name() == 'cli') {
+            $prefix = getenv('PWD') . '/class';
+        } else {
+            $prefix = $_SERVER['DOCUMENT_ROOT'] . 'exercise/class';
+        }
+
+        $fullPath =
+            $prefix .
+            DIRECTORY_SEPARATOR .
+            strtolower(str_replace('\\', '/', $class)) . '.class.php';
+        if (file_exists($fullPath)) {
+            include "$fullPath";
+            $found = true;
+        } else {
+            die("Class $class not found in $fullPath" . PHP_EOL);
+        }
+    }
+}
+
+// vim:set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This script implements 2 funcionalities:
+ * - show a form to add a new user
+ * - insert a new user from POST data
+ */
+
+namespace Simplon\Promo\Exercise;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . 'exercise/autoloader.php';
+
+PageHandler::showPage();
+
+// vim:set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
